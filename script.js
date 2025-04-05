3e7567f9056d04d58c494d4c378273e53e9d1ad2
Arno Pan <apan11@my.bcit.ca>
2025-04-05 15:10:13 -0700

    render X and O

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 console.log("Hello World!");
 
 const container = document.getElementById("container");
-const cells = document.querySelectorAll(".cell");
 
+let board = Array(9).fill(0); 
+const cells = document.querySelectorAll('.cell');
+let currentPlayer = 1;
 
+const dummyBoard = [
+    1, 2, 1,
+    2, 1, 2,
+    2, 1, 2
+];
+
+board = dummyBoard;
+
+const renderBoard = () => {
+    cells.forEach((cell, index) => {
+        if(board[index] === 1) {
+            cell.textContent = "X";
+        }
+        else if(board[index] === 2) {
+            cell.textContent = "O";
+        }
+        else {
+            cell.textContent = "";
+        }
+    })
+}
+renderBoard();
